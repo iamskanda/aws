1246fcf01cc4b5403b5d8380970438291d0cd6fd
root <root@ip-172-31-7-198.ap-south-1.compute.internal>
2023-12-07 14:55:58 +0000

    changing the folder name

---
+++ QAF/qa1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+i=1
+while read name; do 
+
+	wordCount=`echo $name | wc -w`
+	wc=`echo $name | wc -c`
+
+	echo "line count :- $i, word count :- $wordCount, cha : $wc "
+
+
+	let i++
+
+done < $1
+
+echo "skanda"
+echo "skanda"
+
+
+
